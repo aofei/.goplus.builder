2176ae583645203f9dab7ee9aa1382ea130f8d04
Aofei Sheng <aofei@aofeisheng.com>
2024-12-24 11:27:47 +0800

    refactor(tools/spxls): return all definition identifiers in `textDocument/documentLink`
    Signed-off-by: Aofei Sheng <aofei@aofeisheng.com>

--- tools/spxls/internal/server/document.go
+++ tools/spxls/internal/server/document.go
@@ -1,6 +1,11 @@
 package server
 
-import "errors"
+import (
+	"errors"
+	"go/types"
+
+	gopast "github.com/goplus/gop/ast"
+)
 
 // See https://microsoft.github.io/language-server-protocol/specifications/lsp/3.18/specification#textDocument_documentLink
 func (s *Server) textDocumentDocumentLink(params *DocumentLinkParams) ([]DocumentLink, error) {
@@ -16,6 +21,7 @@ func (s *Server) textDocumentDocumentLink(params *DocumentLinkParams) ([]Documen
 	}
 
 	var links []DocumentLink
+
 	for refKey, refs := range result.spxResourceRefs {
 		for _, ref := range refs {
 			startPos := result.fset.Position(ref.Node.Pos())
@@ -35,10 +41,58 @@ func (s *Server) textDocumentDocumentLink(params *DocumentLinkParams) ([]Documen
 			})
 		}
 	}
-	return links, nil
-}
 
-// See https://microsoft.github.io/language-server-protocol/specifications/lsp/3.18/specification#documentLink_resolve
-func (s *Server) documentLinkResolve(params *DocumentLink) (*DocumentLink, error) {
-	return params, nil // No additional resolution is needed at this time.
+	gopast.Inspect(astFile, func(node gopast.Node) bool {
+		if node == nil || !node.Pos().IsValid() {
+			return true
+		}
+		ident, ok := node.(*gopast.Ident)
+		if !ok {
+			return true
+		}
+		obj := result.typeInfo.ObjectOf(ident)
+		if obj == nil {
+			return true
+		}
+
+		var defID SpxDefinitionIdentifier
+		if obj.Pkg() == nil {
+			def := GetSpxBuiltinDefinition(obj.Name())
+			defID = def.ID
+		} else {
+			switch obj := obj.(type) {
+			case *types.Func:
+				defs := NewSpxDefinitionsForFunc(result, obj, result.inferSelectorTypeNameForIdent(ident))
+				if len(defs) > 0 {
+					defID = defs[0].ID
+				}
+			case *types.Var:
+				def := NewSpxDefinitionForVar(result, obj, result.inferSelectorTypeNameForIdent(ident))
+				defID = def.ID
+			case *types.Const:
+				def := NewSpxDefinitionForConst(result, obj)
+				defID = def.ID
+			case *types.TypeName:
+				def := NewSpxDefinitionForType(result, obj)
+				defID = def.ID
+			case *types.PkgName:
+				def := NewSpxDefinitionForPkg(result, obj)
+				defID = def.ID
+			default:
+				return true
+			}
+		}
+
+		target := URI(defID.String())
+		links = append(links, DocumentLink{
+			Range: Range{
+				Start: FromGopTokenPosition(result.fset.Position(ident.Pos())),
+				End:   FromGopTokenPosition(result.fset.Position(ident.End())),
+			},
+			Target: &target,
+		})
+		return true
+	})
+
+	return links, nil
 }
